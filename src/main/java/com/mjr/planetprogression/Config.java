6613ac0395cca013c0d48215886141ceaa5c619d
MJRLegends <MJRLegends@users.noreply.github.com>
2018-01-31 22:35:25 +0000

    Edited config comments

--- src/main/java/com/mjr/planetprogression/Config.java
+++ src/main/java/com/mjr/planetprogression/Config.java
@@ -11,7 +11,7 @@ public class Config {
 	public static void load() {
 		Configuration config = new Configuration(new File(Constants.CONFIG_FILE));
 		config.load();
-		researchMode = config.get(Constants.CONFIG_CATEGORY_GENERAL_SETTINGS, "Research Mode", 0, "Research Modes: 0 - Basic Research Paper Method").getInt(0);
+		researchMode = config.get(Constants.CONFIG_CATEGORY_GENERAL_SETTINGS, "Research Mode", 0, "Research Modes: 0 - Basic Research Paper Method | 2 - Not Used | 3 - Basic Satellite Research Method").getInt(0);
 		generateResearchPaperInLoot = config.get(Constants.CONFIG_CATEGORY_GENERAL_SETTINGS, "Add Research Papers to Dungeon Loot", true, "Will add the Research Papers to spawn in Vanilla Dungeon Loot").getBoolean(true);
 		config.save();
 	}
