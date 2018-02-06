7f81f8a546a654a119a7ad7179b278ab7ee8f8d6
MJRLegends <MJRLegends@users.noreply.github.com>
2018-02-06 00:58:27 +0000

    Fixed meta issue with some recipes

--- src/main/java/com/mjr/planetprogression/recipes/PlanetProgression_Recipes.java
+++ src/main/java/com/mjr/planetprogression/recipes/PlanetProgression_Recipes.java
@@ -24,9 +24,9 @@ public class PlanetProgression_Recipes {
 		}
 		if (Config.researchMode == 2 || Config.researchMode == 3) {
 			RecipeUtilities.addOreRecipe(new ItemStack(PlanetProgression_Blocks.SATTLLITE_BUILDER), new Object[] { "SSS", "WAW", "SSS", 'S', new ItemStack(GCItems.basicItem, 1, 8), 'A', new ItemStack(GCItems.basicItem, 1, 14), 'W',
-					new ItemStack(GCBlocks.aluminumWire, 1, 14) });
+					new ItemStack(GCBlocks.aluminumWire, 1, 1) });
 			RecipeUtilities.addOreRecipe(new ItemStack(PlanetProgression_Blocks.SATTLLITE_CONTROLLER), new Object[] { "STS", "WAW", "STS", 'S', new ItemStack(GCItems.basicItem, 1, 8), 'A', new ItemStack(GCItems.basicItem, 1, 14), 'W',
-					new ItemStack(GCBlocks.aluminumWire, 1, 14), 'T', new ItemStack(PlanetProgression_Items.satelliteBasic, 1) });
+					new ItemStack(GCBlocks.aluminumWire, 1, 1), 'T', new ItemStack(PlanetProgression_Items.satelliteBasic, 1) });
 		}
 		if (Config.researchMode == 3) {
 			MachineRecipeManager.addRecipe(new ItemStack(PlanetProgression_Items.satelliteSurface), new ItemStack[] { new ItemStack(GCItems.basicItem, 1, 1), new ItemStack(GCItems.basicItem, 1, 1),
