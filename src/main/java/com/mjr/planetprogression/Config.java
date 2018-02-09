ed8562e72e039ad9338e93a70d8a222b8f1e76fc
MJRLegends <MJRLegends@users.noreply.github.com>
2018-02-09 21:26:30 +0000

    *Added config option for loot spawn weight in Custom Structure *Few fixes for Custom Structure

--- src/main/java/com/mjr/planetprogression/Config.java
+++ src/main/java/com/mjr/planetprogression/Config.java
@@ -7,6 +7,7 @@ import net.minecraftforge.common.config.Configuration;
 public class Config {
 	public static int researchMode = 0;
 	public static int worldgenStructureAmount = 0;
+	public static int worldgenStructureLootAmount = 0;
 	public static boolean generateResearchPaperInLoot;
 	public static boolean generateResearchPaperInStructure;
 	public static double telescopeTimeModifier = 0.0F;
@@ -17,6 +18,7 @@ public class Config {
 		config.load();
 		researchMode = config.get(Constants.CONFIG_CATEGORY_GENERAL_SETTINGS, "Research Mode", 2, "Research Modes: 1 - Basic Research Paper Method | 2 - Basic Satellite Research Method").getInt(2);
 		worldgenStructureAmount = config.get(Constants.CONFIG_CATEGORY_GENERAL_SETTINGS, "World Gen Structure Weight", 100, "Will be 1 in x (x = being the number in this config option), Default: 100").getInt(100);
+		worldgenStructureLootAmount = config.get(Constants.CONFIG_CATEGORY_GENERAL_SETTINGS, "World Gen Structure Loot Spawn Weight", 5, "Will be 1 in x (x = being the number in this config option), Default: 5").getInt(5);
 		generateResearchPaperInLoot = config.get(Constants.CONFIG_CATEGORY_GENERAL_SETTINGS, "Add Research Papers to Dungeon Loot", false, "Will add the Research Papers to spawn in Vanilla Dungeon Loot").getBoolean(false);
 		generateResearchPaperInStructure = config.get(Constants.CONFIG_CATEGORY_GENERAL_SETTINGS, "Add Research Papers to Custom WorldGen Structure", true, "Will add the Research Papers to spawn in Custom WorldGen Structure, Note will disable structure if set to false").getBoolean(true);
 		telescopeTimeModifier = config.get(Constants.CONFIG_CATEGORY_GENERAL_SETTINGS, "Telescope Research Time Modifier", 0.0, "Default: 0.0F").getDouble(0.0);
