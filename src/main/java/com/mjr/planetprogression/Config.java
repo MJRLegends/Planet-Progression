a61e335e39327317f6a3f5cfc7681c2201237872
MJRLegends <MJRLegends@users.noreply.github.com>
2018-02-09 21:06:06 +0000

    Forgotten change in last commit

--- src/main/java/com/mjr/planetprogression/Config.java
+++ src/main/java/com/mjr/planetprogression/Config.java
@@ -15,7 +15,7 @@ public class Config {
 		config.load();
 		researchMode = config.get(Constants.CONFIG_CATEGORY_GENERAL_SETTINGS, "Research Mode", 2, "Research Modes: 1 - Basic Research Paper Method | 2 - Basic Satellite Research Method").getInt(2);
 		worldgenStructureAmount = config.get(Constants.CONFIG_CATEGORY_GENERAL_SETTINGS, "World Gen Structure Weight", 100, "Will be 1 in x (x = being the number in this config option), Default: 100").getInt(100);
-		generateResearchPaperInLoot = config.get(Constants.CONFIG_CATEGORY_GENERAL_SETTINGS, "Add Research Papers to Dungeon Loot", true, "Will add the Research Papers to spawn in Vanilla Dungeon Loot").getBoolean(false);
+		generateResearchPaperInLoot = config.get(Constants.CONFIG_CATEGORY_GENERAL_SETTINGS, "Add Research Papers to Dungeon Loot", false, "Will add the Research Papers to spawn in Vanilla Dungeon Loot").getBoolean(false);
 		generateResearchPaperInStructure = config.get(Constants.CONFIG_CATEGORY_GENERAL_SETTINGS, "Add Research Papers to Custom WorldGen Structure", true, "Will add the Research Papers to spawn in Custom WorldGen Structure, Note will disable structure if set to false").getBoolean(true);
 		config.save();
 		if(researchMode != 1 && researchMode != 2)
