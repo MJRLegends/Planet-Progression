2f6a02715c210340b70318b530a410e844e5773b
MJRLegends <MJRLegends@users.noreply.github.com>
2018-02-09 19:03:40 +0000

    Added telescope recipe

--- src/main/java/com/mjr/planetprogression/recipes/PlanetProgression_Recipes.java
+++ src/main/java/com/mjr/planetprogression/recipes/PlanetProgression_Recipes.java
@@ -2,6 +2,7 @@ package com.mjr.planetprogression.recipes;
 
 import micdoodle8.mods.galacticraft.core.GCBlocks;
 import micdoodle8.mods.galacticraft.core.GCItems;
+import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 
@@ -13,17 +14,19 @@ import com.mjr.planetprogression.item.PlanetProgression_Items;
 public class PlanetProgression_Recipes {
 
 	public static void init() {
-		registerMachineRecipes();
+		registerRecipes();
 		if (Config.researchMode == 2 || Config.researchMode == 3)
 			SatelliteRocketRecipes.registerRocketCraftingRecipe();
 	}
 
-	private static void registerMachineRecipes() {
+	private static void registerRecipes() {
+		RecipeUtilities.addShapedRecipe(new ItemStack(PlanetProgression_Blocks.TELESCOPE), new Object[] { "TDT", "SGS", "TDT", 'T', new ItemStack(GCItems.basicItem, 1, 7), 'D', new ItemStack(Blocks.DIAMOND_BLOCK), 'G',
+				new ItemStack(Blocks.GLASS_PANE), 'S', new ItemStack(GCItems.basicItem, 1, 8) });
 		if (Config.researchMode == 2) {
 			MachineRecipeManager.addRecipe(new ItemStack(PlanetProgression_Items.satelliteBasic), new ItemStack[] { new ItemStack(GCItems.basicItem, 12, 1), new ItemStack(GCItems.basicItem, 12, 1),
 					new ItemStack(PlanetProgression_Items.satelliteBasicModule, 6, 0) });
-			RecipeUtilities.addShapedRecipe(new ItemStack(PlanetProgression_Items.satelliteBasicModule), new Object[] { "WAW", "CGC", "WAW", 'G', new ItemStack(Items.DYE, 1, 2), 'C', new ItemStack(GCItems.basicItem, 1, 10), 'A', new ItemStack(GCItems.basicItem, 1, 14), 'W',
-				new ItemStack(GCBlocks.aluminumWire, 1, 1) });
+			RecipeUtilities.addShapedRecipe(new ItemStack(PlanetProgression_Items.satelliteBasicModule), new Object[] { "WAW", "CGC", "WAW", 'G', new ItemStack(Items.DYE, 1, 2), 'C', new ItemStack(GCItems.basicItem, 1, 10), 'A',
+					new ItemStack(GCItems.basicItem, 1, 14), 'W', new ItemStack(GCBlocks.aluminumWire, 1, 1) });
 		}
 		if (Config.researchMode == 2 || Config.researchMode == 3) {
 			RecipeUtilities.addShapedRecipe(new ItemStack(PlanetProgression_Blocks.SATTLLITE_BUILDER), new Object[] { "SSS", "WAW", "SSS", 'S', new ItemStack(GCItems.basicItem, 1, 8), 'A', new ItemStack(GCItems.basicItem, 1, 14), 'W',
