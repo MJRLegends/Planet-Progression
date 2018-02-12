6e178beb6c5d2c3172d3a3248a12dc57dbc55885
MJRLegends <MJRLegends@users.noreply.github.com>
2018-02-12 02:00:15 +0000

    Small fix in Constants class

--- src/main/java/com/mjr/planetprogression/Constants.java
+++ src/main/java/com/mjr/planetprogression/Constants.java
@@ -11,7 +11,7 @@ public class Constants {
 	public static final String modVersion = "1.11.2" + "-" + LOCALMAJVERSION + "." + LOCALMINVERSION + "." + LOCALBUILDVERSION;
 
 	public static final String MCVERSION = "[1.11.2]";
-	public static final String DEPENDENCIES_FORGE = "required-after:Forge@[13.20.1.2513,); "; // Keep the space at the end!
+	public static final String DEPENDENCIES_FORGE = "required-after:forge@[13.20.1.2513,); "; // Keep the space at the end!
 	public static final String DEPENDENCIES_MODS = "required-after:galacticraftcore@[4.0.1.133,); required-after:galacticraftplanets@[4.0.1.133,); required-after:mjrlegendslib@[1.11.2-1.0.4,); after:extraplanets; after:moreplanets;"; // Keep the space at the end!
 
 	public static final String CERTIFICATEFINGERPRINT = "b02331787272ec3515ebe63ecdeea0d746653468";
