6103b2a43b79da15bdeab544d1c0a9fca7dadedf
MJRLegends <MJRLegends@users.noreply.github.com>
2018-02-18 18:14:22 +0000

    Changed JEI Support Icons from being letters to item icons

--- src/main/java/com/mjr/planetprogression/jei/PlanetProgressionJEI.java
+++ src/main/java/com/mjr/planetprogression/jei/PlanetProgressionJEI.java
@@ -2,11 +2,14 @@ package com.mjr.planetprogression.jei;
 
 import javax.annotation.Nonnull;
 
+import net.minecraft.item.ItemStack;
 import mezz.jei.api.BlankModPlugin;
 import mezz.jei.api.IGuiHelper;
 import mezz.jei.api.IModRegistry;
 import mezz.jei.api.JEIPlugin;
+import micdoodle8.mods.galacticraft.core.GCBlocks;
 
+import com.mjr.planetprogression.blocks.PlanetProgression_Blocks;
 import com.mjr.planetprogression.jei.satelliteBuilder.SatelliteBuilderRecipeCategory;
 import com.mjr.planetprogression.jei.satelliteBuilder.SatelliteBuilderRecipeHandler;
 import com.mjr.planetprogression.jei.satelliteBuilder.SatelliteBuilderRecipeMaker;
@@ -20,5 +23,9 @@ public class PlanetProgressionJEI extends BlankModPlugin {
 		registry.addRecipeHandlers(new SatelliteRocketRecipeHandler(), new SatelliteBuilderRecipeHandler());
 		registry.addRecipes(SatelliteRocketRecipeMaker.getRecipesList());
 		registry.addRecipes(SatelliteBuilderRecipeMaker.getRecipesList());
+		
+		ItemStack nasaWorkbench = new ItemStack(GCBlocks.nasaWorkbench);
+        registry.addRecipeCategoryCraftingItem(nasaWorkbench, RecipeCategories.SATELLITE_ROCKET_ID);
+        registry.addRecipeCategoryCraftingItem(new ItemStack(PlanetProgression_Blocks.SATTLLITE_BUILDER), RecipeCategories.SATELLITE_ROCKET_ID);
 	}
 }
