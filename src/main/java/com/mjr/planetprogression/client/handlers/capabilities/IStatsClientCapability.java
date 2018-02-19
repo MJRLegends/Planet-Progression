a8f434c15b2a0fb310d1230cca42d5b3ce0818a6
MJRLegends <MJRLegends@users.noreply.github.com>
2018-02-19 19:47:36 +0000

    Added command removed a unlocked Celestial Body or all unlocked Celestial Bodies

--- src/main/java/com/mjr/planetprogression/client/handlers/capabilities/IStatsClientCapability.java
+++ src/main/java/com/mjr/planetprogression/client/handlers/capabilities/IStatsClientCapability.java
@@ -13,9 +13,13 @@ public interface IStatsClientCapability {
 
 	void addUnlockedPlanets(CelestialBody unlockedPlanet);
 
+	void removeUnlockedPlanets(CelestialBody unlockedPlanet);
+	
 	ArrayList<SatelliteData> getSatellites();
 
 	void setSatellites(ArrayList<SatelliteData> satellites);
 
 	void addSatellites(SatelliteData satellites);
+	
+	void removeSatellites(SatelliteData satellites);
 }
