3348baf38ff8e04e4ffc1cc776a6bfd51f0a772c
MJRLegends <MJRLegends@users.noreply.github.com>
2018-02-25 19:56:15 +0000

    Added tool tip to research papers to say where they need to be used

--- src/main/java/com/mjr/planetprogression/item/ResearchPaper.java
+++ src/main/java/com/mjr/planetprogression/item/ResearchPaper.java
@@ -49,5 +49,6 @@ public class ResearchPaper extends BasicItem {
 				list.add(EnumColor.AQUA + TranslateUtilities.translate("research.paper.woldgen.desc"));
 		} else
 			list.add(EnumColor.AQUA + TranslateUtilities.translate("research.paper.satellite.controller.desc"));
+		list.add(EnumColor.BRIGHT_GREEN + TranslateUtilities.translate("research.paper.use.desc"));
 	}
 }
