dff78c035af6f8508fe94be484295f6c6afb0dff
MJRLegends <MJRLegends@users.noreply.github.com>
2018-04-05 19:50:49 +0100

    Fixed Telescope bounding box, Changed to multi block

--- src/main/java/com/mjr/planetprogression/blocks/PlanetProgression_Blocks.java
+++ src/main/java/com/mjr/planetprogression/blocks/PlanetProgression_Blocks.java
@@ -4,9 +4,10 @@ import java.lang.reflect.Constructor;
 
 import net.minecraft.block.Block;
 import net.minecraft.item.ItemBlock;
-import net.minecraftforge.fml.common.registry.GameRegistry;
 
 import com.google.common.collect.ObjectArrays;
+import com.mjr.mjrlegendslib.itemBlock.ItemBlockDefault;
+import com.mjr.mjrlegendslib.util.RegisterUtilities;
 import com.mjr.planetprogression.Config;
 import com.mjr.planetprogression.Constants;
 import com.mjr.planetprogression.PlanetProgression;
@@ -14,12 +15,14 @@ import com.mjr.planetprogression.itemBlocks.ItemBlockTelescope;
 import com.mjr.planetprogression.tileEntities.TileEntitySatelliteBuilder;
 import com.mjr.planetprogression.tileEntities.TileEntitySatelliteController;
 import com.mjr.planetprogression.tileEntities.TileEntityTelescope;
+import com.mjr.planetprogression.tileEntities.TileEntityTelescopeFake;
 
 public class PlanetProgression_Blocks {
 
 	public static Block TELESCOPE;
 	public static Block SATTLLITE_BUILDER;
 	public static Block SATTLLITE_CONTROLLER;
+	public static Block FAKE_TELESCOPE;
 
 	public static void init() {
 		initBlocks();
@@ -61,6 +64,7 @@ public class PlanetProgression_Blocks {
 
 	public static void initBlocks() {
 		TELESCOPE = new BlockTelescope("telescope");
+		FAKE_TELESCOPE = new BlockTelescopeFake("telescope_fake_block");
 		if (Config.researchMode == 2 || Config.researchMode == 3) {
 			SATTLLITE_BUILDER = new BlockSatelliteBuilder("satellite_builder");
 			SATTLLITE_CONTROLLER = new BlockSatelliteController("satellite_controller");
@@ -69,6 +73,7 @@ public class PlanetProgression_Blocks {
 
 	public static void registerBlocks() throws NoSuchMethodException {
 		registerBlock(TELESCOPE, ItemBlockTelescope.class, TELESCOPE.getUnlocalizedName().substring(5));
+		registerBlock(FAKE_TELESCOPE, ItemBlockDefault.class, FAKE_TELESCOPE.getUnlocalizedName().substring(5));
 		if (Config.researchMode == 2 || Config.researchMode == 3) {
 			registerBlock(SATTLLITE_BUILDER, ItemBlockTelescope.class, SATTLLITE_BUILDER.getUnlocalizedName().substring(5));
 			registerBlock(SATTLLITE_CONTROLLER, ItemBlockTelescope.class, SATTLLITE_CONTROLLER.getUnlocalizedName().substring(5));
@@ -76,10 +81,11 @@ public class PlanetProgression_Blocks {
 	}
 
 	private static void registerTileEntitys() {
-		GameRegistry.registerTileEntity(TileEntityTelescope.class, Constants.modName + "Telescope");
+		RegisterUtilities.registerTileEntity(TileEntityTelescope.class, Constants.modName + "Telescope");
+		RegisterUtilities.registerTileEntity(TileEntityTelescopeFake.class, Constants.modName + "Telescope Fake");
 		if (Config.researchMode == 2 || Config.researchMode == 3) {
-			GameRegistry.registerTileEntity(TileEntitySatelliteBuilder.class, Constants.modName + "SatelliteBuilder");
-			GameRegistry.registerTileEntity(TileEntitySatelliteController.class, Constants.modName + "SatelliteController");
+			RegisterUtilities.registerTileEntity(TileEntitySatelliteBuilder.class, Constants.modName + "SatelliteBuilder");
+			RegisterUtilities.registerTileEntity(TileEntitySatelliteController.class, Constants.modName + "SatelliteController");
 		}
 	}
 
