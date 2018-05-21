eac4c34a52d4f49cbc9a437f28e6f8e0aab00d91
MJRLegends <MJRLegends@users.noreply.github.com>
2018-05-21 16:38:30 +0100

    Released v0.2.4

--- src/main/java/com/mjr/planetprogression/Constants.java
+++ src/main/java/com/mjr/planetprogression/Constants.java
@@ -6,13 +6,13 @@ public class Constants {
 
 	public static final int LOCALMAJVERSION = 0;
 	public static final int LOCALMINVERSION = 2;
-	public static final int LOCALBUILDVERSION = 3;
+	public static final int LOCALBUILDVERSION = 4;
 
 	public static final String modVersion = "1.12.2" + "-" + LOCALMAJVERSION + "." + LOCALMINVERSION + "." + LOCALBUILDVERSION;
 
 	public static final String MCVERSION = "[1.12.2]";
 	public static final String DEPENDENCIES_FORGE = "required-after:forge@[13.20.1.2513,); "; // Keep the space at the end!
-	public static final String DEPENDENCIES_MODS = "required-after:mjrlegendslib@[1.12.2-1.1.1,); required-after:galacticraftcore@[4.0.1.174,); required-after:galacticraftplanets@[4.0.1.174,); after:extraplanets; after:moreplanets;";
+	public static final String DEPENDENCIES_MODS = "required-after:mjrlegendslib@[1.12.2-1.1.1,); required-after:galacticraftcore@[4.0.1.177,); required-after:galacticraftplanets@[4.0.1.177,); after:extraplanets; after:moreplanets;";
 
 	public static final String CERTIFICATEFINGERPRINT = "b02331787272ec3515ebe63ecdeea0d746653468";
 
