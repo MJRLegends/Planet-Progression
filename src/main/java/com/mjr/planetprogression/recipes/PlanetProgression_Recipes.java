259ec135dac2e6bc7a7988d6a22d2b3b9af8d1e0
MJRLegends <MJRLegends@users.noreply.github.com>
2019-04-22 15:25:06 +0100

    Added missing recipes for Satellite Rocket Launch Pad & Satellite Rocket Launch Pad

--- src/main/java/com/mjr/planetprogression/recipes/PlanetProgression_Recipes.java
+++ src/main/java/com/mjr/planetprogression/recipes/PlanetProgression_Recipes.java
@@ -33,6 +33,8 @@ public class PlanetProgression_Recipes {
 					new ItemStack(GCBlocks.aluminumWire, 1, 1) });
 			RecipeUtilities.addShapedRecipe(new ItemStack(PlanetProgression_Blocks.SATTLLITE_CONTROLLER), new Object[] { "STS", "WAW", "STS", 'S', new ItemStack(GCItems.basicItem, 1, 8), 'A', new ItemStack(GCItems.basicItem, 1, 14), 'W',
 					new ItemStack(GCBlocks.aluminumWire, 1, 1), 'T', new ItemStack(PlanetProgression_Items.satelliteBasic, 1) });
+			RecipeUtilities.addShapedRecipe(new ItemStack(PlanetProgression_Blocks.SATTLLITE_LAUNCHER, 1, 0), new Object[] { "ZVZ", "YXY", "ZWZ", 'V', new ItemStack(GCItems.basicItem, 1, 19), 'W', new ItemStack(GCBlocks.aluminumWire, 1, 0), 'X', new ItemStack(GCItems.basicItem, 1, 14), 'Y', new ItemStack(GCItems.basicItem, 1, 9), 'Z', new ItemStack(GCItems.basicItem, 1, 9) });
+			RecipeUtilities.addOreRecipe(new ItemStack(PlanetProgression_Blocks.ADVANCED_LAUCHPAD, 9, 0), new Object[] { "YYY", "YYY", "XXX", 'X', "blockIron", 'Y', "compressedIron" });
 		}
 		if (Config.researchMode == 3) {
 			MachineRecipeManager.addRecipe(new ItemStack(PlanetProgression_Items.satelliteSurface), new ItemStack[] { new ItemStack(GCItems.basicItem, 1, 1), new ItemStack(GCItems.basicItem, 1, 1),
