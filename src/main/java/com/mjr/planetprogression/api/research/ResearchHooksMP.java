64b6b4515e3c8c71800f20899de403e3157e8482
MJRLegends <MJRLegends@users.noreply.github.com>
2019-08-15 19:58:16 +0100

    Added an API for checking if a celestial body is researched/unlocked

---
+++ src/main/java/com/mjr/planetprogression/api/research/ResearchHooksMP.java
@@ -0,0 +1,19 @@
+package com.mjr.planetprogression.api.research;
+
+import com.mjr.planetprogression.handlers.capabilities.CapabilityStatsHandler;
+import com.mjr.planetprogression.handlers.capabilities.IStatsCapability;
+
+import micdoodle8.mods.galacticraft.api.galaxies.CelestialBody;
+import net.minecraft.entity.player.EntityPlayerMP;
+
+public class ResearchHooksMP {
+
+	public boolean hasUnlockedCelestialBody(EntityPlayerMP player, CelestialBody body) {
+		IStatsCapability stats = null;
+		if (player != null) {
+			stats = player.getCapability(CapabilityStatsHandler.PP_STATS_CAPABILITY, null);
+			return stats.getUnlockedPlanets().contains(body);
+		}
+		return false;
+	}
+}
