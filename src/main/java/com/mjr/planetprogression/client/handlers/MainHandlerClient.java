f2609cdc953633cc563a03297a36718925e62b4a
MJRLegends <MJRLegends@users.noreply.github.com>
2019-08-15 19:57:40 +0100

    Allow for ExtraPlanets map to take priority since PlanetProgressions research system is now supported by ExtraPlanets map

--- src/main/java/com/mjr/planetprogression/client/handlers/MainHandlerClient.java
+++ src/main/java/com/mjr/planetprogression/client/handlers/MainHandlerClient.java
@@ -56,10 +56,12 @@ public class MainHandlerClient {
 	@SideOnly(Side.CLIENT)
 	public void onGuiOpenEvent(GuiOpenEvent event) {
 		if (((event.getGui() instanceof GuiCelestialSelection))) {
-			if (GameSettings.isKeyDown(micdoodle8.mods.galacticraft.core.tick.KeyHandlerClient.galaxyMap)) {
-				event.setGui(new CustomGuiCelestialSelection(true, ((GuiCelestialSelection) event.getGui()).possibleBodies, ((GuiCelestialSelection) event.getGui()).canCreateStations));
-			} else {
-				event.setGui(new CustomGuiCelestialSelection(false, ((GuiCelestialSelection) event.getGui()).possibleBodies, ((GuiCelestialSelection) event.getGui()).canCreateStations));
+			if(!event.getGui().getClass().getName().equalsIgnoreCase("com.mjr.extraplanets.client.gui.screen.CustomCelestialSelection")) {
+				if (GameSettings.isKeyDown(micdoodle8.mods.galacticraft.core.tick.KeyHandlerClient.galaxyMap)) {
+					event.setGui(new CustomGuiCelestialSelection(true, ((GuiCelestialSelection) event.getGui()).possibleBodies, ((GuiCelestialSelection) event.getGui()).canCreateStations));
+				} else {
+					event.setGui(new CustomGuiCelestialSelection(false, ((GuiCelestialSelection) event.getGui()).possibleBodies, ((GuiCelestialSelection) event.getGui()).canCreateStations));
+				}
 			}
 		}
 	}
