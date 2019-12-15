5324b16f4544b094a3b9f98dd2fd3f0adce6fda1
MJRLegends <MJRLegends@users.noreply.github.com>
2019-12-15 21:47:52 +0000

    Added config option to disable showing of the Overworld As been Discovered Message on first joining world/server #50

--- src/main/java/com/mjr/planetprogression/Config.java
+++ src/main/java/com/mjr/planetprogression/Config.java
@@ -16,6 +16,8 @@ public class Config {
 	public static String[] worldgenStructurePaperBlacklist;
 	public static String[] dungeonPaperBlacklist;
 
+	public static boolean showOverworldSpawnMessage;
+
 	public static void load() {
 		Configuration config = new Configuration(new File(Constants.CONFIG_FILE));
 		config.load();
@@ -23,13 +25,16 @@ public class Config {
 		worldgenStructureAmount = config.get(Constants.CONFIG_CATEGORY_GENERAL_SETTINGS, "World Gen Structure Weight", 100, "Will be 1 in x (x = being the number in this config option), Default: 100").getInt(100);
 		worldgenStructureLootAmount = config.get(Constants.CONFIG_CATEGORY_GENERAL_SETTINGS, "World Gen Structure Loot Spawn Weight", 10, "Will be 1 in x (x = being the number in this config option), Default: 10").getInt(10);
 		generateResearchPaperInLoot = config.get(Constants.CONFIG_CATEGORY_GENERAL_SETTINGS, "Add Research Papers to Dungeon Loot", false, "Will add the Research Papers to spawn in Vanilla Dungeon Loot").getBoolean(false);
-		generateResearchPaperInStructure = config.get(Constants.CONFIG_CATEGORY_GENERAL_SETTINGS, "Add Research Papers to Custom WorldGen Structure", false,
-				"Will add the Research Papers to spawn in Custom WorldGen Structure, Note will disable structure if set to false").getBoolean(false);
+		generateResearchPaperInStructure = config
+				.get(Constants.CONFIG_CATEGORY_GENERAL_SETTINGS, "Add Research Papers to Custom WorldGen Structure", false, "Will add the Research Papers to spawn in Custom WorldGen Structure, Note will disable structure if set to false")
+				.getBoolean(false);
 		telescopeTimeModifier = config.get(Constants.CONFIG_CATEGORY_GENERAL_SETTINGS, "Telescope Research Time Modifier", 1.0, "Default: 1.0F").getDouble(1.0);
 		satelliteControllerModifier = config.get(Constants.CONFIG_CATEGORY_GENERAL_SETTINGS, "Satellite Controller Research Time Modifier", 1.0, "Default: 1.0F").getDouble(1.0);
-		worldgenStructureWorldWhitelist = config.getStringList("Whitelist for World Gen Structure", Constants.CONFIG_CATEGORY_GENERAL_SETTINGS, new String[]{"0"}, "Default: 0 | Format: 'planet.NAME' OR 'moon.NAME' | Example: 'planet.venus'");
+		worldgenStructureWorldWhitelist = config.getStringList("Whitelist for World Gen Structure", Constants.CONFIG_CATEGORY_GENERAL_SETTINGS, new String[] { "0" }, "Default: 0 | Format: 'planet.NAME' OR 'moon.NAME' | Example: 'planet.venus'");
 		worldgenStructurePaperBlacklist = config.getStringList("Blacklist for Research Papers in World Gen Structure", Constants.CONFIG_CATEGORY_GENERAL_SETTINGS, new String[] {}, "Format: 'planet.NAME' OR 'moon.NAME' | Example: 'planet.venus'");
 		dungeonPaperBlacklist = config.getStringList("Blacklist for Research Papers in Dungeon Loot", Constants.CONFIG_CATEGORY_GENERAL_SETTINGS, new String[] {}, "Format: 'planet.NAME' OR 'moon.NAME' | Example: 'planet.venus'");
+		showOverworldSpawnMessage = config.get(Constants.CONFIG_CATEGORY_GENERAL_SETTINGS, "Show Overworld As been Discovered Message on first joining world/server", true,
+				"Setting to false will disable the 'You have discovered Earth' message you get in chat when first joining a world/server").getBoolean(true);
 
 		config.save();
 		if (researchMode != 1 && researchMode != 2)
