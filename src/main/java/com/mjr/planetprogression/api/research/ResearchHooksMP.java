f0dacf980c76282b02ca71882ccba3cc6de8afb6
MJRLegends <MJRLegends@users.noreply.github.com>
2020-08-10 23:17:45 +0100

    Added api method to check if any bodies have been unlocked in a solar system

--- src/main/java/com/mjr/planetprogression/api/research/ResearchHooksMP.java
+++ src/main/java/com/mjr/planetprogression/api/research/ResearchHooksMP.java
@@ -3,9 +3,12 @@ package com.mjr.planetprogression.api.research;
 import com.mjr.planetprogression.handlers.capabilities.CapabilityStatsHandler;
 import com.mjr.planetprogression.handlers.capabilities.IStatsCapability;
 
-import net.minecraft.entity.player.EntityPlayerMP;
-
 import micdoodle8.mods.galacticraft.api.galaxies.CelestialBody;
+import micdoodle8.mods.galacticraft.api.galaxies.GalaxyRegistry;
+import micdoodle8.mods.galacticraft.api.galaxies.Planet;
+import micdoodle8.mods.galacticraft.api.galaxies.SolarSystem;
+
+import net.minecraft.entity.player.EntityPlayerMP;
 
 public class ResearchHooksMP {
 
@@ -15,6 +18,17 @@ public class ResearchHooksMP {
 			stats = player.getCapability(CapabilityStatsHandler.PP_STATS_CAPABILITY, null);
 			return stats.getUnlockedPlanets().contains(body);
 		}
+		return false;
+	}
+
+	public static boolean hasUnlockedBodiesInSystem(EntityPlayerMP player, SolarSystem body) {
+		IStatsCapability stats = null;
+		if (player != null) {
+			stats = player.getCapability(CapabilityStatsHandler.PP_STATS_CAPABILITY, null);
+		}
+		for (Planet planet : GalaxyRegistry.getPlanetsForSolarSystem(body))
+			if (stats.getUnlockedPlanets().contains(planet))
+				return true;
 		return false;
 	}
 }
