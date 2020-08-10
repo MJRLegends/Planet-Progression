62593f4ea44d3f0dd142ac38305805e9eed0c76e
MJRLegends <MJRLegends@users.noreply.github.com>
2020-08-10 16:20:46 +0100

    Added a config option to disable Pre-Researched Bodies unlocked chat message

--- src/main/java/com/mjr/planetprogression/handlers/MainHandlerServer.java
+++ src/main/java/com/mjr/planetprogression/handlers/MainHandlerServer.java
@@ -148,8 +148,8 @@ public class MainHandlerServer {
 				if (list.contains(PlanetProgression.getBodyName(planet.getUnlocalizedName()))) {
 					if (planet != null && !stats.getUnlockedPlanets().contains(planet)) {
 						stats.addUnlockedPlanets(planet);
-						player.sendMessage(new TextComponentString(TranslateUtilities.translate("research.discovered.name") + planet.getLocalizedName() + "!"));
-						player.sendMessage(new TextComponentString(TranslateUtilities.translate("research.howto.name")));
+						if (Config.showPreResearchedMessage)
+							player.sendMessage(new TextComponentString(TranslateUtilities.translate("research.discovered.name") + planet.getLocalizedName() + "!"));
 					}
 				}
 			}
@@ -157,8 +157,8 @@ public class MainHandlerServer {
 				if (list.contains(PlanetProgression.getBodyName(moon.getUnlocalizedName()))) {
 					if (moon != null && !stats.getUnlockedPlanets().contains(moon)) {
 						stats.addUnlockedPlanets(moon);
-						player.sendMessage(new TextComponentString(TranslateUtilities.translate("research.discovered.name") + moon.getLocalizedName() + "!"));
-						player.sendMessage(new TextComponentString(TranslateUtilities.translate("research.howto.name")));
+						if (Config.showPreResearchedMessage)
+							player.sendMessage(new TextComponentString(TranslateUtilities.translate("research.discovered.name") + moon.getLocalizedName() + "!"));
 					}
 				}
 			}
