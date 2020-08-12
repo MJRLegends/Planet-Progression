e629555a42a3836b958e2ef00a709c2f823c5e98
ROM <rom.void95@gmail.com>
2020-08-12 05:42:17 -0500

    Update MainHandlerClient.java

--- src/main/java/com/mjr/planetprogression/client/handlers/MainHandlerClient.java
+++ src/main/java/com/mjr/planetprogression/client/handlers/MainHandlerClient.java
@@ -57,13 +57,12 @@ public class MainHandlerClient {
 		}
 	}
 
-	@SubscribeEvent(priority=EventPriority.LOWEST)
+	@SubscribeEvent(priority = EventPriority.LOWEST)
 	@SideOnly(Side.CLIENT)
 	public void onGuiOpenEvent(GuiOpenEvent event) {
 		if (((event.getGui() instanceof GuiCelestialSelection))) {
-			if(event.getGui().getClass().getName().equalsIgnoreCase("asmodeuscore.core.astronomy.gui.screen.NewGuiCelestialSelection"))
-				MessageUtilities.throwCrashError("Please disable the following option: enableNewGalaxyMap in configs/AsmodeusCore/core.conf");
-			else if(!Loader.isClassAvailable("com.mjr.extraplanets.compatibility.PlanetProgressionCompatibility") || !event.getGui().getClass().getName().equalsIgnoreCase("com.mjr.extraplanets.client.gui.screen.CustomCelestialSelection")) {
+			if(!event.getGui().getClass().getName().equalsIgnoreCase("asmodeuscore.core.astronomy.gui.screen.NewGuiCelestialSelection"))
+			if(!Loader.isClassAvailable("com.mjr.extraplanets.compatibility.PlanetProgressionCompatibility") || !event.getGui().getClass().getName().equalsIgnoreCase("com.mjr.extraplanets.client.gui.screen.CustomCelestialSelection") || !event.getGui().getClass().getName().equalsIgnoreCase("asmodeuscore.core.astronomy.gui.screen.NewGuiCelestialSelection")) {
 				if (GameSettings.isKeyDown(micdoodle8.mods.galacticraft.core.tick.KeyHandlerClient.galaxyMap)) {
 					event.setGui(new CustomGuiCelestialSelection(true, ((GuiCelestialSelection) event.getGui()).possibleBodies, ((GuiCelestialSelection) event.getGui()).canCreateStations));
 				} else {
